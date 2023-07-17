4250a1ebf2770988bcd2f4d32b76002837dff084
afuza <afuzapratama@gmail.com>
2023-07-18 01:44:25 +0900

    GREEN_UPDATE V0.3
    add home messege please update

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -21,4 +21,8 @@ router.post('/api/auth/register', register);
 router.delete('/api/auth/logout', verifyToken, logout);
 router.post('/api/auth/refToken', refresh_Token);
 
+router.get('/', (req, res) => {
+    res.json({ for: 'Hello World!' });
+});
+
 module.exports = router;
