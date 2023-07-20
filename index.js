5ac9ae87239a18ad6c5ce1bd80e53383017783bd
afuza <afuzapratama@gmail.com>
2023-07-21 06:44:50 +0900

    UPDATE BETA

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: [process.env.DOMAIN_ORIGIN, 'http://localhost:3000', 'https://hack.9six.io'], exposedHeaders: 'Set-Cookie', credentials: true }));
+app.use(cors({ origin: process.env.DOMAIN_ORIGIN, exposedHeaders: 'Set-Cookie', credentials: true },));
 
 // Routes
 app.use(EmailRoute);
