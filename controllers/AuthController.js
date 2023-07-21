6552939705fb564edbc55e5232dc233391305c40
afuza <afuzapratama@gmail.com>
2023-07-21 19:35:25 +0900

    BETA 5

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -61,9 +61,7 @@ const login = async (req, res) => {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
-
-
-        res.status(200).json({ accessToken });
+        res.status(201).json({ accessToken });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
